f1aacebee14311004491927dfdacb30d413dadc5
nayoonho <soldier4443@gmail.com>
2019-05-19 01:53:02 +0900

    Feat: Location 데이터 받아와서 맵 초기화

--- app/src/main/java/com/poop9/poop/MapFragment.kt
+++ app/src/main/java/com/poop9/poop/MapFragment.kt
@@ -3,24 +3,39 @@ package com.poop9.poop
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
 import android.content.Context
+import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.observe
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.poop9.poop.base.BaseFragment
 import com.poop9.poop.databinding.FragmentMapBinding
+import com.poop9.poop.model.LocationData
 import pub.devrel.easypermissions.EasyPermissions
 
 
-class MapFragment : BaseFragment(), OnMapReadyCallback {
+@SuppressLint("MissingPermission")
+class MapFragment : BaseFragment() {
     override val layoutId: Int
         get() = R.layout.fragment_map
 
     private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     private lateinit var binding: FragmentMapBinding
+
+    private val _location = MutableLiveData<LocationData>()
+    val location: LiveData<LocationData>
+        get() = _location
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -37,18 +52,39 @@ class MapFragment : BaseFragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMapBinding.bind(view)
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        val activity = activity ?: return
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+        fusedLocationClient.lastLocation
+            .addOnFailureListener(Exception::printStackTrace)
+            .addOnSuccessListener { location: Location? ->
+                location?.let(LocationData.Companion::from)?.let {
+                    _location.postValue(it)
+                }
+            }
+
+        location.observe(this) { locationData ->
+            loadMap(locationData)
+        }
     }
 
-    @SuppressLint("MissingPermission")
-    override fun onMapReady(googleMap: GoogleMap?) {
-        map = googleMap ?: return
-        map.mapType = GoogleMap.MAP_TYPE_NORMAL
-        map.isMyLocationEnabled = true
-
-        map.setOnMyLocationClickListener { location ->
-            Log.d("MapFragment", "$location")
+    private fun loadMap(locationData: LocationData) {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            map = googleMap
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            map.isMyLocationEnabled = true
+            changeCamera(createCameraUpdate(locationData))
         }
+    }
+
+    private fun createCameraUpdate(locationData: LocationData): CameraUpdate =
+        CameraPosition.Builder()
+            .target(locationData.toLatLng())
+            .zoom(15f)
+            .build()
+            .let(CameraUpdateFactory::newCameraPosition)
+
+    private fun changeCamera(update: CameraUpdate) {
+        map.moveCamera(update)
     }
 }
