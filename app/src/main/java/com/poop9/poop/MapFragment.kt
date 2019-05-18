c622a84e0f3831205074f144d545375c580aa868
nayoonho <soldier4443@gmail.com>
2019-05-18 23:08:57 +0900

    Feat: MapFragment 추가

---
+++ app/src/main/java/com/poop9/poop/MapFragment.kt
@@ -0,0 +1,8 @@
+package com.poop9.poop
+
+import com.poop9.poop.base.BaseFragment
+
+class MapFragment : BaseFragment() {
+    override val layoutId: Int
+        get() = R.layout.fragment_map
+}
---
+++ app/src/main/java/com/poop9/poop/base/BaseFragment.kt
@@ -0,0 +1,17 @@
+package com.poop9.poop.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+    abstract val layoutId: Int
+        @LayoutRes get
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layoutId, container, false)
+    }
+}
