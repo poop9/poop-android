ac231eccf769aeee8acb7f6528557420df0027d4
nayoonho <soldier4443@gmail.com>
2019-05-19 03:56:36 +0900

    Feat: Fragment 매번 재생성되는 것 막음

--- app/src/main/java/com/poop9/poop/MainActivity.kt
+++ app/src/main/java/com/poop9/poop/MainActivity.kt
@@ -3,8 +3,10 @@ package com.poop9.poop
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.poop9.poop.databinding.ActivityMainBinding
 import com.poop9.poop.map.MapFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = bind<ActivityMainBinding>(R.layout.activity_main)
 
-        binding.bottomNavigation.setOnNavigationItemSelectedListener(this::handleMenuItemSelected)
-        binding.bottomNavigation.setOnNavigationItemReselectedListener {} // For prevent recreating fragments
+        binding.bottomNavigation.let { nav ->
+            nav.setOnNavigationItemSelectedListener(this::handleMenuItemSelected)
+            nav.setOnNavigationItemReselectedListener {} // For prevent recreating fragments
+        }
 
         showMapFragment()
     }
@@ -29,10 +33,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showMapFragment() {
-        replace(R.id.fragment_container, MapFragment())
+        switchFragment(MapFragment(), "map")
     }
 
     private fun showReportFragment() {
-        replace(R.id.fragment_container, ReportFragment())
+        switchFragment(ReportFragment(), "report")
+    }
+
+    // Prevent re-creating fragments
+    private fun switchFragment(fragment: Fragment, tag: String) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        val curFrag = supportFragmentManager.primaryNavigationFragment
+        if (curFrag != null) fragmentTransaction.hide(curFrag)
+
+        var fr = supportFragmentManager.findFragmentByTag(tag)
+        if (fr != null) {
+            fragmentTransaction.show(fr)
+        } else {
+            fr = fragment
+            fragmentTransaction.add(R.id.fragment_container, fragment, tag)
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fr)
+        fragmentTransaction.setReorderingAllowed(true)
+        fragmentTransaction.commit()
     }
 }
