a897e2c7908b4ed7ee1c20209b4ef17f1f4e68c8
nayoonho <soldier4443@gmail.com>
2019-05-18 22:16:32 +0900

    Feat: Bottom Navigation에 따라서 Fragment 다르게 보여주도록 설정

---
+++ app/src/main/java/com/poop9/poop/Ext.kt
@@ -0,0 +1,46 @@
+package com.poop9.poop
+
+import android.app.Activity
+import android.content.Intent
+import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+fun <T : ViewDataBinding> Activity.bind(@LayoutRes layoutId: Int, initializer: (T.() -> Unit)? = null): T =
+    DataBindingUtil.setContentView<T>(this, layoutId).apply {
+        initializer?.invoke(this)
+    }
+
+inline fun <reified T : Activity> Activity.startActivity(intentModifier: (Intent).() -> Unit = {}) {
+    Intent(this, T::class.java).let {
+        intentModifier(it)
+        startActivity(it)
+    }
+}
+
+/**
+ * Example:
+ * replaceWhen(R.id.fragment_container, MyFragment()) {
+ *     a == 4
+ * }
+ */
+fun FragmentActivity.replaceWhen(
+    @IdRes containerId: Int, fragment: Fragment,
+    predicate: () -> Boolean
+) {
+    if (predicate())
+        replace(containerId, fragment)
+}
+
+/**
+ * Example:
+ * replace(R.id.fragment_container, MyFragment())
+ */
+fun FragmentActivity.replace(@IdRes containerId: Int, fragment: Fragment) {
+    supportFragmentManager.beginTransaction()
+        .replace(containerId, fragment)
+        .commitNow()
+}
